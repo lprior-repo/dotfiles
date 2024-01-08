e194bcdcefac74ff382ee9b12975994a465418cc
lprior-repo <56145286+lprior-repo@users.noreply.github.com>
2024-01-08 14:09:41 -0600

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,19 +1,17 @@
- status is-interactive
+Check if the shell is interactive
+if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Set an empty greeting message
 set fish_greeting ""
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # User configuration
-function cd
-    z $argv
+alias ls='eza --long --header --git --all --group-directories-first --color=auto --icons'
+alias cat='bat --style=numbers,changes,header,grid --paging=never --color=always'
+function __zoxide_cd
+    cd $argv
+    and commandline -f repaint
 end
-
-function ls
-    eza --long --header --git --all --group-directories-first --color=auto --icons $argv
-end
-
-function cat
-    bat --style=numbers,changes,header,grid --paging=never --color=always $argv
-end
+zoxide init fish --cmd cd | source
