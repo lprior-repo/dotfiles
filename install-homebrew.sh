3a0316c169f12e318087f56f6e10116a53a381a5
lprior-repo <56145286+lprior-repo@users.noreply.github.com>
2025-01-19 06:53:11 -0600

    Update install-homebrew.sh

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # https://rapidprogrammer.com/how-to-install-homebrew-with-ansible
 yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo >> /home/lprior/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/lprior/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
