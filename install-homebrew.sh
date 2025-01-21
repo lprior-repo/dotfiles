fe6246fb94243e11c3fe8a634a2c5508736d2b17
lprior-repo <56145286+lprior-repo@users.noreply.github.com>
2025-01-21 12:15:16 -0600

    Delete install-homebrew.sh

--- install-homebrew.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# https://rapidprogrammer.com/how-to-install-homebrew-with-ansible
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
