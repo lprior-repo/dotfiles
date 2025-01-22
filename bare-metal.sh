3300cc8d5f2df6bb6e08cba5966a7f6f6b2ab894
lprior-repo <56145286+lprior-repo@users.noreply.github.com>
2025-01-22 05:26:59 -0600

    Update bare-metal.sh

--- bare-metal.sh
+++ bare-metal.sh
@@ -14,7 +14,7 @@ else
     exit 1
 fi
 echo "Install Brew"
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Setup bashrc"
     echo >> /home/lprior/.bashrc
